4c1089f507aa94a7ac99bc949340a42de981380c
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-09 13:41:52 +0100

    test: NFT (ERC721 token) missing test

--- test/NFT.ts
+++ test/NFT.ts
@@ -1,4 +1,40 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("NFT", function () {});
+describe("NFT", function () {
+  const TOKEN_NAME = "Github NFT";
+  const TOKEN_SYMBOL = "GH";
+  const MAX_SUPPLY = 3;
+
+  async function deploy() {
+    const GithubToken = await ethers.getContractFactory("NFT");
+    const githubToken = await GithubToken.deploy(TOKEN_NAME, TOKEN_SYMBOL);
+    const [owner, ...otherAccounts] = await ethers.getSigners();
+    await githubToken.deployed();
+    return { githubToken, owner, otherAccounts };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the name and symbol of the token", async function () {
+      const { githubToken } = await deploy();
+      expect(await githubToken.name()).to.equal(TOKEN_NAME);
+      expect(await githubToken.symbol()).to.equal(TOKEN_SYMBOL);
+    });
+    it("Should be no tokens yet", async function () {
+      const { githubToken } = await deploy();
+      expect(await githubToken.totalSupply()).to.equal(0);
+    });
+  });
+
+  describe("Mint", function () {
+    it("Should mint all the tokens", async function () {
+      const { githubToken, owner } = await deploy();
+      await githubToken.mint(owner.address);
+      await githubToken.mint(owner.address);
+      await githubToken.mint(owner.address);
+      console.log(await githubToken.totalSupply());
+      expect(await githubToken.totalSupply()).to.equal(MAX_SUPPLY);
+      expect(await githubToken.balanceOf(owner.address)).to.equal(MAX_SUPPLY);
+    });
+  });
+});

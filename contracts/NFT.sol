c5d1d31356d15ec85d0f923be69e91e089f7f550
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-09 13:42:20 +0100

    feature: NFT smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,4 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract NFT {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721, ERC721Enumerable {
+    using Counters for Counters.Counter;
+
+    uint256 MAX_SUPPLY = 3;
+    Counters.Counter private _tokenId;
+
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {}
+
+    function mint(address to) public {
+        uint256 tokenId = _tokenId.current();
+        require(tokenId < MAX_SUPPLY, "No NFTs left");
+        _tokenId.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
